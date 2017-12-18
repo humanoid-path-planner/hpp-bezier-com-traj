c2a90b965b788cb413e4dc9a2a43a99541544312
Pierre Fernbach <pierre.fernbach@laas.fr>
2017-12-18 11:43:31 +0100

    quadProg : take an 'unbounded' output as a success instead of a failure.
    Maybe need a specific case

--- src/common_solve_methods.cpp
+++ src/common_solve_methods.cpp
@@ -191,7 +191,8 @@ ResultData solve(Cref_matrixXX A, Cref_vectorX ci0, Cref_matrixXX H, Cref_vector
     VectorX x = initGuess;
     tsid::solvers::EiquadprogFast_status status = QPsolver.solve_quadprog(H,g,CE,ce0,CI,ci0,x);
     ResultData res;
-    res.success_ = status == tsid::solvers::EIQUADPROG_FAST_OPTIMAL;
+    res.success_ = (status == tsid::solvers::EIQUADPROG_FAST_OPTIMAL || status == tsid::solvers::EIQUADPROG_FAST_UNBOUNDED);
+    std::cout<<"quad_prog status : "<<status<<std::endl;
     if(res.success_)
     {
         assert (!(is_nan(x)));
