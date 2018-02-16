c3f121653da3027237b5d5c4ce938b777fc3ac3f
Pierre Fernbach <pierre.fernbach@laas.fr>
2018-02-16 15:54:12 +0100

    [transition test] add equation for constraint : c0 dc0 x c1

--- src/solve_transition.cpp
+++ src/solve_transition.cpp
@@ -16,7 +16,7 @@
 #define DDC0_CONSTRAINT 0
 #endif
 #ifndef DC1_CONSTRAINT
-#define DC1_CONSTRAINT 1
+#define DC1_CONSTRAINT 0
 #endif
 #ifndef USE_SLACK
 #define USE_SLACK 0
@@ -52,6 +52,90 @@ void printQHullFile(const std::pair<MatrixXX, VectorX>& Ab,VectorX intPoint,cons
      file.close();
 }
 
+
+#if (!DDC0_CONSTRAINT && !DC1_CONSTRAINT)
+
+/// ### EQUATION FOR CONSTRAINTS ON INIT AND FINAL POSITION AND VELOCITY (DEGREE = 4)
+/** @brief evaluateCurveAtTime compute the expression of the point on the curve at t, defined by the waypoint pi and one free waypoint (x)
+ * @param pi constant waypoints of the curve, assume p0 p1 x p2 p3
+ * @param t param (normalized !)
+ * @return the expression of the waypoint such that wp.first . x + wp.second = point on curve
+ */
+coefs_t evaluateCurveAtTime(std::vector<point_t> pi,double t){
+    coefs_t wp;
+    double t2 = t*t;
+    double t3 = t2*t;
+    // equation found with sympy
+    wp.first = -3.0*t3 + 3.0*t2;
+    wp.second = -1.0*pi[0]*t3 + 3.0*pi[0]*t2 - 3.0*pi[0]*t + 1.0*pi[0] + 3.0*pi[1]*t3 - 6.0*pi[1]*t2 + 3.0*pi[1]*t + 1.0*pi[3]*t3;
+   // std::cout<<"wp at t = "<<t<<std::endl;
+   // std::cout<<" first : "<<wp.first<<" ; second : "<<wp.second.transpose()<<std::endl;
+    return wp;
+}
+
+coefs_t evaluateAccelerationCurveAtTime(std::vector<point_t> pi,double T,double t){
+    coefs_t wp;
+    double alpha = 1./(T*T);
+    // equation found with sympy
+    wp.first = (-18.0*t + 6.0)*alpha;
+    wp.second = (-6.0*pi[0]*t + 6.0*pi[0] + 18.0*pi[1]*t - 12.0*pi[1] + 6.0*pi[3]*t)*alpha;
+   // std::cout<<"acc_wp at t = "<<t<<std::endl;
+   // std::cout<<" first : "<<wp.first<<" ; second : "<<wp.second.transpose()<<std::endl;
+    return wp;
+}
+
+
+std::vector<point_t> computeConstantWaypoints(const ProblemData& pData,double T){
+    // equation for constraint on initial and final position and velocity (degree 4, 4 constant waypoint and one free (p2))
+    // first, compute the constant waypoints that only depend on pData :
+    int n = 3;
+    std::vector<point_t> pi;
+    pi.push_back(pData.c0_); //p0
+    pi.push_back((pData.dc0_ * T / n )+  pData.c0_); // p1
+    pi.push_back(point_t::Zero()); // p2 = x
+    pi.push_back(pData.c1_); // p3
+
+    return pi;
+}
+
+std::vector<waypoint6_t> computeWwaypoints(const ProblemData& pData,double T){
+    std::vector<waypoint6_t> wps;
+    std::vector<point_t> pi = computeConstantWaypoints(pData,T);
+    std::vector<Matrix3> Cpi;
+    for(int i = 0 ; i < pi.size() ; ++i){
+        Cpi.push_back(skew(pi[i]));
+    }
+    const Vector3 g = pData.contacts_.front().contactPhase_->m_gravity;
+    const Matrix3  Cg = skew( g);
+    const double T2 = T*T;
+    const double alpha = 1/(T2);
+    // equation of waypoints for curve w found with sympy
+    waypoint6_t w0 = initwp<waypoint6_t>();
+    w0.first.block<3,3>(0,0) = 6*alpha*Matrix3::Identity();
+    w0.first.block<3,3>(3,0) = 6.0*Cpi[0]*alpha;
+    w0.second.head<3>() = (6*pi[0] - 12*pi[1])*alpha;
+    w0.second.tail<3>() = 1.0*(1.0*Cg*T2*pi[0] - 12.0*Cpi[0]*pi[1])*alpha;
+    wps.push_back(w0);
+    waypoint6_t w1 = initwp<waypoint6_t>();
+    w1.first.block<3,3>(3,0) = 1.0*(-6.0*Cpi[0] + 6.0*Cpi[1])*alpha;
+    w1.second.head<3>() = 1.0*(4.0*pi[0] - 6.0*pi[1] + 2.0*pi[3])*alpha;
+    w1.second.tail<3>() = 1.0*(1.0*Cg*T2*pi[1] + 2.0*Cpi[0]*pi[3])*alpha;
+    wps.push_back(w1);
+    waypoint6_t w2 = initwp<waypoint6_t>();
+    w2.first.block<3,3>(0,0) = -6.0*alpha*Matrix3::Identity();
+    w2.first.block<3,3>(3,0) = 1.0*(1.0*Cg*T2 - 6.0*Cpi[1])*alpha;
+    w2.second.head<3>() = 1.0*(2.0*pi[0] + 4.0*pi[3])*alpha;
+    w2.second.tail<3>() = 1.0*(-2.0*Cpi[0]*pi[3] + 6.0*Cpi[1]*pi[3])*alpha;
+    wps.push_back(w2);
+    waypoint6_t w3 = initwp<waypoint6_t>();
+    w3.first.block<3,3>(0,0) = -12*alpha*Matrix3::Identity();
+    w3.first.block<3,3>(3,0) = -12.0*Cpi[3]*alpha;
+    w3.second.head<3>() = (6*pi[1] + 6*pi[3])*alpha;
+    w3.second.tail<3>() = 1.0*(1.0*Cg*T2*pi[3] - 6.0*Cpi[1]*pi[3])*alpha;
+    wps.push_back(w3);
+    return wps;
+}
+#endif // degree 3 : c0 dc0 x c1
 
 #if (!DDC0_CONSTRAINT && DC1_CONSTRAINT)
 
