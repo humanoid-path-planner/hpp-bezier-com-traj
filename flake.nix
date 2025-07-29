d1071a5abbbb1ec4b926f41ce3450bbd9314c1f3
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-07-29 15:49:45 +0200

    Nix: update gepetto/nix use

--- flake.nix
+++ flake.nix
@@ -2,32 +2,33 @@
   description = "Multi contact trajectory generation for the COM using Bezier curves";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/refs/pull/362956/head";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
+    gepetto.url = "github:gepetto/nix";
+    flake-parts.follows = "gepetto/flake-parts";
+    nixpkgs.follows = "gepetto/nixpkgs";
+    nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
+    systems.follows = "gepetto/systems";
+    treefmt-nix.follows = "gepetto/treefmt-nix";
   };
 
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
+      systems = import inputs.systems;
+      imports = [ inputs.gepetto.flakeModule ];
       perSystem =
-        { pkgs, self', ... }:
         {
-          devShells.default = pkgs.mkShell { inputsFrom = [ self'.packages.default ]; };
+          lib,
+          pkgs,
+          self',
+          ...
+        }:
+        {
           packages = {
             default = self'.packages.hpp-bezier-com-traj;
-            hpp-bezier-com-traj = pkgs.hpp-bezier-com-traj.overrideAttrs (_: {
-              src = pkgs.lib.fileset.toSource {
+            hpp-bezier-com-traj = pkgs.python3Packages.hpp-bezier-com-traj.overrideAttrs {
+              src = lib.fileset.toSource {
                 root = ./.;
-                fileset = pkgs.lib.fileset.unions [
+                fileset = lib.fileset.unions [
                   ./CMakeLists.txt
                   ./include
                   ./package.xml
@@ -36,7 +37,7 @@
                   ./tests
                 ];
               };
-            });
+            };
           };
         };
     };
