01dd6b3f6ef92cc00b1060a3dfb47f5825fedd7f
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-11-08 15:06:00 +0100

    flake: sync with gepetto/nix template

--- flake.nix
+++ flake.nix
@@ -12,33 +12,37 @@
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
-      imports = [ inputs.gepetto.flakeModule ];
-      perSystem =
-        {
-          lib,
-          pkgs,
-          self',
-          ...
-        }:
-        {
-          packages = {
-            default = self'.packages.hpp-bezier-com-traj;
-            hpp-bezier-com-traj = pkgs.python3Packages.hpp-bezier-com-traj.overrideAttrs {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./CMakeLists.txt
-                  ./include
-                  ./package.xml
-                  ./python
-                  ./src
-                  ./tests
-                ];
-              };
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, self, ... }:
+      {
+        systems = import inputs.systems;
+        imports = [
+          inputs.gepetto.flakeModule
+          { gepetto-pkgs.overlays = [ self.overlays.default ]; }
+        ];
+        flake.overlays.default = _final: prev: {
+          hpp-bezier-com-traj = prev.hpp-bezier-com-traj.overrideAttrs {
+            src = lib.fileset.toSource {
+              root = ./.;
+              fileset = lib.fileset.unions [
+                ./CMakeLists.txt
+                ./include
+                ./package.xml
+                ./python
+                ./src
+                ./tests
+              ];
             };
           };
         };
-    };
+        perSystem =
+          { pkgs, self', ... }:
+          {
+            packages = {
+              default = self'.packages.hpp-bezier-com-traj;
+              hpp-bezier-com-traj = pkgs.python3Packages.hpp-bezier-com-traj;
+            };
+          };
+      }
+    );
 }
